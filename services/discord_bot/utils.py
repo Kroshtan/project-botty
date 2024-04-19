bc188b8dbf8066d9e0067df66942558ab90248f6
Floris van Beers <31923442+Kroshtan@users.noreply.github.com>
2024-04-19 22:11:48 +0200

    5 normalize urls before storing as content (#22)
    * Issue 5 - normalize urls before storing content
    
    * Issue 5 - block adding same content url twice

--- services/discord_bot/utils.py
+++ services/discord_bot/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from urllib3.util import parse_url
 
 async def purge_reactions(message, member, current_reaction_emoji):
     for reaction in message.reactions:
@@ -13,3 +13,14 @@ def cosine_dist(source_embedding, target_embedding):
     # scale to [0, 1]
     cosine_sim_scaled = (cosine_similarity + 1) / 2
     return 1 - cosine_sim_scaled
+
+
+def normalize_url(url: str) -> str:
+    url_parts = parse_url(url)
+    if url_parts.host is None:
+        raise ValueError(f"Not a properly formed url: {url}")
+    clean_url = f"{url_parts.scheme if url_parts.scheme else 'http'}://"
+    clean_url += url_parts.host
+    clean_url = clean_url.rstrip("/")
+    clean_url += f"{url_parts.path if url_parts.path else ''}"
+    return clean_url
